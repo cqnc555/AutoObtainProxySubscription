fea0606eb8a87044a7652ee7fedfee744f33033c
Administrator <771959111@qq.com>
2024-09-26 23:40:06 +0800

    fix

--- page.bat
+++ page.bat
@@ -1,3 +1,5 @@
 E:\PythonDelevlop\python-3.10.11-embed-amd64\Scripts\pyinstaller.exe -F subscription_simple.py
 
+
+E:\Delevlop\PortableGit-2.38.1-64-bit.7z\git-bash.exe
 git config --global http.proxy 'http://127.0.0.1:10809'
